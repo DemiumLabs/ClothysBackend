8328c7b953bbb36920f95205afe914aba09e266e
David Rios Benet <davidrivers93@gmail.com>
2017-09-27 16:44:37 +0200

    Give away 1000 coins on user registration

--- common/models/customer.js
+++ common/models/customer.js
@@ -2,4 +2,15 @@
 
 module.exports = function(Customer) {
 
+  Customer.observe('after save', function(ctx,next){
+    if(ctx.isNewInstance){
+      let Seat = Customer.app.models.Seat;
+
+      Seat.create({price: 1000, buyerId: ctx.instance.id},(err,seatInstance ) => {
+        if(err) next(err,null);
+        next();
+      });
+    }
+  });
+
 };
