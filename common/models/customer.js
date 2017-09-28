e7f99d5a7d7e2381b4fd4478e6834d10f4d4cc56
David Rios Benet <davidrivers93@gmail.com>
2017-09-28 10:51:08 +0200

    Create remote method for calculate user amount

--- common/models/customer.js
+++ common/models/customer.js
@@ -2,6 +2,8 @@
 
 module.exports = function(Customer) {
 
+  var Promise = require('promise');
+
   Customer.observe('after save', function(ctx,next){
     if(ctx.isNewInstance){
       let Seat = Customer.app.models.Seat;
@@ -11,6 +13,31 @@ module.exports = function(Customer) {
         next();
       });
     }
+  });
+
+  Customer.prototype.calculate = function(cb) {
+    let Seat = Customer.app.models.Seat;
+    let promises = []
+    promises.push(Seat.find({where: {buyerId : this.id }}));
+    promises.push(Seat.find({where: {sellerId : this.id}}));
+
+    Promise.all(promises).then((result) => {
+      let buyers = result[0];
+      let sellers = result[1];
+      let customerAmount = 0;
+
+      buyers.forEach(buy => customerAmount += buy.price);
+      sellers.forEach(sell => customerAmount -= sell.price);
+      
+      cb(null, customerAmount);
+    });
+
+  }
+
+  Customer.remoteMethod('calculate', {
+        isStatic: false,
+        returns: {arg: 'customerAmount', type: 'number', root: true},
+        http: {verb: 'get'}
   });
 
 };
