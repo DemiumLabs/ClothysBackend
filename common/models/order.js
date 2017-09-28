0997edbc0dce3d964bbd3428ef0d9a111ab778f8
David Rios Benet <davidrivers93@gmail.com>
2017-09-28 11:18:24 +0200

    Return an error if the Item is selled

--- common/models/order.js
+++ common/models/order.js
@@ -2,4 +2,16 @@
 
 module.exports = function(Order) {
 
+  Order.observe('before save', function(ctx,next) {
+    ctx.instance.item((err,item) => {
+      if(!item.isSelled){
+        item.updateAttribute("isSelled",true).then((result) => {
+          next()
+        });
+      }else{
+        next("Ya esta vendido el articulo",null);
+      }
+    });
+  });
+
 };
